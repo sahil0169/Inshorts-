758c77a79b5df1186b2f4fe658dbe58f1a8dbdfa
sahil0169 <87486544+sahil0169@users.noreply.github.com>
2023-12-01 12:37:47 +0530

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Header() {
-    return (
-        <div className="head1">
-         <img src="logo_inshorts.png" className="head3" />
-        </div>
-    )
-}
